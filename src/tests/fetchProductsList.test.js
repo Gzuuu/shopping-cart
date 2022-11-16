da6f8cfeafd3be5d254f46501c9f41957ca9f99e
jesue <jesue_gege@hotmail.com>
2022-11-16 16:54:20 -0300

    1 e 2 concluida

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,29 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    const expected = typeof fetchProductsList;
+    const result = 'function'
+    expect(expected).toBe(result)
   });
 
   it('fetch é chamado ao executar fetchProductsList', () => {
-
+    const result = fetch('');
+    expect(fetchProductsList('computador')).toEqual(result)
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    const fet = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+    const data = await fet.json();
+    const expected = await data.results;
+    expect(expected).toEqual(await fetchProductsList('computador'))
   });
 
-  // it('...', () => {
-  // });
+  it('testa se chamada a função fetchProductList com o argumento computador o resultado é igual ao objeto computadorSearch', async ()=> {
+    const expected = await fetchProductsList('computador')
+    expect(expected).toEqual(computadorSearch)
+  })
+
+  it('Testa se ao chamar a função fetchProductsList sem parametros é retornado um erro', async () => {
+    await expect(fetchProductsList()).resolves.toEqual('Termo de busca não informado')
+  });
 });
