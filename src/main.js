184a6c040e13507487a3afcbb71d9f3e75f37ec3
jesue <jesue_gege@hotmail.com>
2022-11-16 19:24:05 -0300

    4 concluida

--- src/main.js
+++ src/main.js
@@ -5,18 +5,33 @@ import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
-async function productGenerator() {
+const father = document.querySelector('.products');
+
+function createLoading() {
+  const text = document.createElement('h1');
+  text.setAttribute('class', 'loading');
+  text.innerText = 'carregando...';
+  father.appendChild(text);
+}
+
+function removeLoading() {
+  const text = document.querySelector('.loading');
+  text.remove(father);
+}
+
+async function createProducts() {
+  createLoading();
   const produtos = await fetchProductsList('computador');
+  removeLoading();
   produtos.forEach((produto) => {
-    const pai = document.querySelector('.products');
     const obj = {
       id: produto.id,
       title: produto.title,
       thumbnail: produto.thumbnail,
       price: produto.price,
     };
-    pai.appendChild(createProductElement(obj));
+    father.appendChild(createProductElement(obj));
   });
 }
 
-productGenerator();
+createProducts();
